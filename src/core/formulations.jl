339b389e163a793023849e3f59788b04062b861b
rodrigomha <rodrigomha@gmail.com>
2023-05-02 16:43:23 -0700

    update formulations

--- src/core/formulations.jl
+++ src/core/formulations.jl
@@ -9,21 +9,37 @@ abstract type AbstractEnergyManagement <: AbstractStorageFormulation end
 
 """
 Formulation type to add basic storage formulation. With `attributes=Dict("reservation"=>true)` the formulation is augmented
-with abinary variable to prevent simultanious charging and discharging
+with a binary variable to prevent simultanious charging and discharging
 """
-struct BookKeeping <: AbstractStorageFormulation end
+struct StorageDispatchEnergyOnly <: AbstractStorageFormulation end
+
 """
 Formulation type to add storage formulation than can provide ancillary services. With `attributes=Dict("reservation"=>true)` the formulation is augmented
-with abinary variable to prevent simultanious charging and discharging
+with a binary variable to prevent simultaneous charging and discharging
 """
-struct BatteryAncillaryServices <: AbstractStorageFormulation end
+struct StorageDispatch <: AbstractStorageFormulation end
+
 """
 Formulation type to add storage formulation that respects end of horizon energy state of charge target. With `attributes=Dict("reservation"=>true)` the formulation is augmented
-with abinary variable to prevent simultanious charging and discharging
+with a binary variable to prevent simultaneous charging and discharging
 """
 struct EnergyTarget <: AbstractEnergyManagement end
 
-struct StorageDispatch <: PSI.AbstractStorageFormulation end
-struct EnergyTargetAncillaryServices <: PSI.AbstractEnergyManagement end
-struct EnergyValue <: PSI.AbstractEnergyManagement end
-struct EnergyValueCurve <: PSI.AbstractEnergyManagement end
+"""
+Formulation type to add storage formulation that respects end of horizon energy state of charge target that can provide ancillary services.
+With `attributes=Dict("reservation"=>true)` the formulation is augmented with a binary variable to prevent simultaneous charging and discharging
+"""
+struct EnergyTargetAncillaryServices <: AbstractEnergyManagement end
+
+"""
+To do DOCS
+"""
+struct EnergyValue <: AbstractEnergyManagement end
+
+"""
+To do DOCS
+"""
+struct EnergyValueCurve <: AbstractEnergyManagement end
+
+const BookKeeping = StorageDispatchEnergyOnly
+const BatteryAncillaryServices = StorageDispatch
