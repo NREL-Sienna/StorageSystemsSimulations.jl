328f6b2291bbd5f71136c5f51c3110ac1c2d6d36
Jose Daniel Lara <jdlara@berkeley.edu>
2023-07-11 17:27:26 -0600

    move files around

--- src/storage_models.jl
+++ src/storage_models.jl
@@ -548,3 +548,155 @@ function PSI.objective_function!(
     PSI.add_proportional_cost!(container, PSI.EnergyShortageVariable(), devices, T())
     return
 end
+
+
+function PSI._add_variable_cost_to_objective!(
+    container::PSI.OptimizationContainer,
+    ::T,
+    component::U,
+    op_cost::Union{Nothing, PSY.TwoPartCost},
+    ::V,
+) where {T <: PSI.EnergyVariable, U <: PSY.Storage, V <: EnergyValue}
+    component_name = PSY.get_name(component)
+    @debug "Market Bid" _group = PSI.LOG_GROUP_COST_FUNCTIONS component_name
+    time_steps = PSI.get_time_steps(container)
+    base_power = PSI.get_base_power(container)
+    param = PSI.get_parameter(container, EnergyValueTimeSeriesParameter(), U)
+    multiplier =
+        PSI.get_parameter_multiplier_array(container, EnergyValueTimeSeriesParameter(), U)
+
+    for t in time_steps
+        _param = PSI.get_parameter_column_values(param, component_name)
+        variable = PSI.get_variable(container, T(), U)[component_name, t]
+        lin_cost = variable * _param[t] * multiplier[component_name, t] * base_power
+        PSI.add_to_objective_variant_expression!(container, lin_cost)
+    end
+
+    return
+end
+
+function PSI._add_variable_cost_to_objective!(
+    container::PSI.OptimizationContainer,
+    ::T,
+    component::U,
+    op_cost::Union{Nothing, PSY.TwoPartCost},
+    ::V,
+) where {T <: PSI.EnergyVariable, U <: PSY.Storage, V <: ChargingValue}
+    component_name = PSY.get_name(component)
+    @debug "Market Bid" _group = PSI.LOG_GROUP_COST_FUNCTIONS component_name
+    time_steps = PSI.get_time_steps(container)
+    base_power = PSI.get_base_power(container)
+    param = PSI.get_parameter(container, ChargingValueTimeSeriesParameter(), U)
+    multiplier =
+        PSI.get_parameter_multiplier_array(container, ChargingValueTimeSeriesParameter(), U)
+
+    for t in time_steps
+        _param = PSI.get_parameter_column_values(param, component_name)
+        variable = PSI.get_variable(container, T(), U)[component_name, t]
+        lin_cost = variable * _param[t] * multiplier[component_name, t] * base_power
+        PSI.add_to_objective_variant_expression!(container, lin_cost)
+    end
+
+    return
+end
+
+function PSI._add_variable_cost_to_objective!(
+    container::PSI.OptimizationContainer,
+    ::T,
+    component::U,
+    op_cost::DynamicEnergyCost,
+    ::V,
+) where {T <: PSI.EnergyVariable, U <: PSY.Storage, V <: EnergyValueCurve}
+    component_name = PSY.get_name(component)
+    @debug "Market Bid" _group = PSI.LOG_GROUP_COST_FUNCTIONS component_name
+    time_steps = PSI.get_time_steps(container)
+    initial_time = PSI.get_initial_time(container)
+    variable_cost_forecast = PSY.get_variable_cost(
+        component,
+        op_cost;
+        start_time=initial_time,
+        len=length(time_steps),
+    )
+    variable_cost_forecast_values = TimeSeries.values(variable_cost_forecast)
+    parameter_container = PSI._get_cost_function_parameter_container(
+        container,
+        PSI.CostFunctionParameter(),
+        component,
+        T(),
+        V(),
+        eltype(variable_cost_forecast_values),
+    )
+    pwl_cost_expressions =
+        PSI._add_pwl_term!(container, component, variable_cost_forecast_values, T(), V())
+    jump_model = PSI.get_jump_model(container)
+    for t in time_steps
+        PSI.set_parameter!(
+            parameter_container,
+            jump_model,
+            PSY.get_cost(variable_cost_forecast_values[t]),
+            # Using 1.0 here since we want to reuse the existing code that adds the mulitpler
+            #  of base power times the time delta.
+            1.0,
+            component_name,
+            t,
+        )
+        PSI.add_to_expression!(
+            container,
+            PSI.ProductionCostExpression,
+            pwl_cost_expressions[t],
+            component,
+            t,
+        )
+        PSI.add_to_objective_variant_expression!(container, pwl_cost_expressions[t])
+    end
+
+    return
+end
+
+function PSI.add_to_expression!(
+    container::PSI.OptimizationContainer,
+    expressions::Vector,
+    ::Type{U},
+    devices::Union{Vector{V}, IS.FlattenIteratorWrapper{V}},
+    model::PSI.ServiceModel{X, W},
+) where {
+    U <: PSI.VariableType,
+    V <: PSY.Component,
+    X <: PSY.Reserve{PSY.ReserveUp},
+    W <: PSI.AbstractReservesFormulation,
+}
+    for expr in expressions
+        PSI.add_to_expression!(container, expr, U, devices, model)
+    end
+    return
+end
+
+function PSI.add_to_expression!(
+    container::PSI.OptimizationContainer,
+    ::Type{T},
+    ::Type{U},
+    devices::Union{Vector{V}, IS.FlattenIteratorWrapper{V}},
+    model::PSI.ServiceModel{X, W},
+) where {
+    T <: ReserveEnergyExpressionUB,
+    U <: PSI.VariableType,
+    V <: PSY.Storage,
+    X <: PSY.Reserve{PSY.ReserveUp},
+    W <: PSI.AbstractReservesFormulation,
+}
+    service_name = PSI.get_service_name(model)
+    variable = PSI.get_variable(container, U(), X, service_name)
+    if !PSI.has_container_key(container, T, V)
+        PSI.add_expressions!(container, T, devices, model)
+    end
+    expression = PSI.get_expression(container, T(), V)
+    for d in devices, t in PSI.get_time_steps(container)
+        service = get_service(d, service_name)
+        time_frame = PSY.get_time_frame(service)
+        # need to confirm that the  time frame is in min ?
+        time_delta = time_frame / PSI._get_minutes_per_period(container)
+        name = PSY.get_name(d)
+        PSI._add_to_jump_expression!(expression[name, t], variable[name, t], 1.0)
+    end
+    return
+end
