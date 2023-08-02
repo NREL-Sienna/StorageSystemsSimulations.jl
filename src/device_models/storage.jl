884c1fb3c5ec135f9dbb4d74268c192fbe41dbb1
Jose Daniel Lara <jdlara@berkeley.edu>
2023-08-01 23:09:37 -0600

    more fixes

--- src/device_models/storage.jl
+++ src/device_models/storage.jl
@@ -508,3 +508,83 @@ function PSI.objective_function!(
     PSI.add_proportional_cost!(container, StorageEnergyShortageVariable(), devices, T())
     return
 end
+
+function PSI._add_feedforward_arguments!(
+    container::OptimizationContainer,
+    model::DeviceModel,
+    devices::IS.FlattenIteratorWrapper{T},
+    ff::PSI.EnergyTargetFeedforward,
+) where {T <: PSY.Storage}
+    parameter_type = PSI.get_default_parameter_type(ff, T)
+    add_parameters!(container, parameter_type, ff, model, devices)
+    # Enabling this FF requires the addition of an extra variable
+    add_variables!(container, StorageEnergyShortageVariable, devices, PSI.get_formulation(model)())
+    return
+end
+
+@doc raw"""
+        add_feedforward_constraints(
+            container::OptimizationContainer,
+            ::DeviceModel,
+            devices::IS.FlattenIteratorWrapper{T},
+            ff::EnergyTargetFeedforward,
+        ) where {T <: PSY.Component}
+
+Constructs a equality constraint to a fix a variable in one model using the variable value from other model results.
+
+
+``` variable[var_name, t] + slack[var_name, t] >= param[var_name, t] ```
+
+# LaTeX
+
+`` x + slack >= param``
+
+# Arguments
+* container::OptimizationContainer : the optimization_container model built in PowerSimulations
+* model::DeviceModel : the device model
+* devices::IS.FlattenIteratorWrapper{T} : list of devices
+* ff::EnergyTargetFeedforward : a instance of the FixValue Feedforward
+"""
+function PSI.add_feedforward_constraints!(
+    container::OptimizationContainer,
+    ::DeviceModel{T, U},
+    devices::IS.FlattenIteratorWrapper{T},
+    ff::PSI.EnergyTargetFeedforward,
+) where {T <: PSY.Storage, U <: AbstractStorageFormulation}
+    time_steps = PSI.get_time_steps(container)
+    parameter_type = PSI.get_default_parameter_type(ff, T)
+    param = PSI.get_parameter_array(container, parameter_type(), T)
+    multiplier = PSI.get_parameter_multiplier_array(container, parameter_type(), T)
+    target_period = ff.target_period
+    penalty_cost = ff.penalty_cost
+    for var in PSI.et_affected_values(ff)
+        variable = PSI.get_variable(container, var)
+        slack_var = PSI.get_variable(container, StorageEnergyShortageVariable(), T)
+        set_name, set_time = JuMP.axes(variable)
+        IS.@assert_op set_name == [PSY.get_name(d) for d in devices]
+        IS.@assert_op set_time == time_steps
+
+        var_type = PSI.get_entry_type(var)
+        con_ub = PSI.add_constraints_container!(
+            container,
+            PSI.FeedforwardEnergyTargetConstraint(),
+            T,
+            set_name;
+            meta = "$(var_type)target",
+        )
+
+        for d in devices
+            name = PSY.get_name(d)
+            con_ub[name] = JuMP.@constraint(
+                container.JuMPmodel,
+                variable[name, target_period] + slack_var[name, target_period] >=
+                param[name, target_period] * multiplier[name, target_period]
+            )
+            PSI.add_to_objective_invariant_expression!(
+                container,
+                slack_var[name, target_period] * penalty_cost,
+            )
+        end
+    end
+    return
+end
