1dd34fee3baeae0a5b3006fd6b487ddbbfd66ff1
Jose Daniel Lara <jdlara@berkeley.edu>
2023-09-07 10:33:06 -0600

    add new constraints

--- src/core/constraints.jl
+++ src/core/constraints.jl
@@ -1,13 +1,20 @@
-struct ReserveEnergyConstraint <: PSI.ConstraintType end
+### Define Constraints using PSI.ConstraintType ###
 
-#= To add constraint types later
+struct StatusOutOn <: PSI.ConstraintType end
+struct StatusInOn <: PSI.ConstraintType end
 
-struct EnergyBalanceConstraint <: PSI.ConstraintType end
-struct EnergyBudgetConstraint <: PSI.ConstraintType end
-struct EnergyBudgetConstraint <: PSI.ConstraintType end
-struct EnergyCapacityDownConstraint <: PSI.ConstraintType end
-struct EnergyCapacityUpConstraint <: PSI.ConstraintType end
-struct EnergyTargetConstraint <: PSI.ConstraintType end
-struct RangeLimitConstraint <: PSI.ConstraintType end
+## AS Provision
+struct ReserveCoverageConstraint <: PSI.ConstraintType end
+struct ReserveCoverageConstraintEndOfPeriod <: PSI.ConstraintType end
+struct ChargingReservePowerLimit <: PSI.ConstraintType end
+struct DischargingReservePowerLimit <: PSI.ConstraintType end
 
-=#
+## Auxiliary for Output
+struct AuxiliaryReserveConstraint <: PSI.ConstraintType end
+struct ReserveBalance <: PSI.ConstraintType end
+
+struct BatteryStatusChargeOn <: PSI.ConstraintType end
+struct BatteryStatusDischargeOn <: PSI.ConstraintType end
+struct BatteryBalance <: PSI.ConstraintType end
+struct CyclingCharge <: PSI.ConstraintType end
+struct CyclingDischarge <: PSI.ConstraintType end
