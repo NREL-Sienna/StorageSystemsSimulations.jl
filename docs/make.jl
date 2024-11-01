5579245962dd06efaf16119a840280d58e4c9850
kdayday <kdayday@users.noreply.github.com>
2024-11-01 09:58:45 -0600

    Enforce strict compilation and reorg/rename APIs

--- docs/make.jl
+++ docs/make.jl
@@ -12,14 +12,14 @@ pages = OrderedDict(
     #"Introductory Background" => ["background/discretization.md", "background/ancillary_services.md"],
     "Formulation Library" =>
         Any["StorageDispatchWithReserves" => "formulation_library/StorageDispatchWithReserves.md",],
-    "Code Base Developer Guide" => "code_base_developer_guide/developer.md",
+    "Code Base Developer Guide" => ["code_base_developer_guide/developer.md",
+        "code_base_developer_guide/internals.md"],
     "API Reference" => "api/StorageSystemsSimulations.md",
 )
 
 makedocs(;
     modules=[StorageSystemsSimulations],
     format=Documenter.HTML(; prettyurls=haskey(ENV, "GITHUB_ACTIONS")),
-    warnonly=[:missing_docs],
     sitename="StorageSystemsSimulations.jl",
     authors="Jose Daniel Lara, Rodrigo Henriquez-Auba, Sourabh Dalvi",
     pages=Any[p for p in pages],
