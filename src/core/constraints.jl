7044777f257eb5e9d632f8addf49b5d86afbd9e7
rodrigomha <rodrigomha@gmail.com>
2024-11-06 13:44:57 -0800

    add docstring

--- src/core/constraints.jl
+++ src/core/constraints.jl
@@ -7,6 +7,15 @@
 
 struct StateofChargeTargetConstraint <: PSI.ConstraintType end
 
+"""
+Struct to create the constraints to limit the state of charge of the storage system.
+
+The specified constraints are given by:
+
+```math
+0 \\leq e^{st}_{t}\\leq E^{max}_{st} \\quad \\forall t \\in \\mathcal{T}
+```
+"""
 struct StateofChargeLimitsConstraint <: PSI.ConstraintType end
 struct StorageCyclingCharge <: PSI.ConstraintType end
 struct StorageCyclingDischarge <: PSI.ConstraintType end
@@ -18,6 +27,15 @@ struct ReserveCoverageConstraint <: PSI.ConstraintType end
 struct ReserveCoverageConstraintEndOfPeriod <: PSI.ConstraintType end
 struct ReserveCompleteCoverageConstraint <: PSI.ConstraintType end
 struct ReserveCompleteCoverageConstraintEndOfPeriod <: PSI.ConstraintType end
+"""
+TODO: Rodrigo
+"""
 struct StorageTotalReserveConstraint <: PSI.ConstraintType end
+"""
+TODO: Rodrigo
+"""
 struct StorageRegularizationConstraintCharge <: PSI.ConstraintType end
+"""
+TODO: Rodrigo
+"""
 struct StorageRegularizationConstraintDischarge <: PSI.ConstraintType end
