6e293f4b0e9588cfd4262f036b40133732884ae6
annacasavant <anca7065@colorado.edu>
2024-12-19 08:22:38 -0700

    updating make.jl to reflect diataxis and added interlink

--- docs/make.jl
+++ docs/make.jl
@@ -2,26 +2,33 @@ using Documenter
 using PowerSystems
 using StorageSystemsSimulations
 using DataStructures
+using DocumenterInterLinks
 
 pages = OrderedDict(
     "Welcome Page" => "index.md",
     "Quick Start Guide" => "quick_start_guide.md",
     "Tutorials" =>
-        Any["tutorials/single_stage_model.md", "tutorials/simulation_tutorial.md"],
-    #"How-to and common uses" => ["how_to/configure_feedforward.md"],
-    #"Introductory Background" => ["background/discretization.md", "background/ancillary_services.md"],
-    "Formulation Library" =>
-        Any["StorageDispatchWithReserves" => "formulation_library/StorageDispatchWithReserves.md",],
-    "Code Base Developer Guide" => [
-        "code_base_developer_guide/developer.md",
-        "code_base_developer_guide/internals.md",
-    ],
-    "API Reference" => "api/StorageSystemsSimulations.md",
+        Any["tutorials/single_stage_model.md"],
+    "Explanation" => "explanation/stub.md",
+    "How-to-Guides" => "how_to/stub.md",
+    "Reference" => Any[
+        "Formulation Library" => "reference/formulation_library/StorageDispatchWithReserves.md",
+        "Developers" => "reference/developers/code_base_developer_guide/developer.md",
+        "API" => "reference/api/StorageSystemsSimulations.md"],
+    
 )
 
+links = InterLinks(
+    "PowerSystems" => (
+        "https://nrel-sienna.github.io/PowerSystems.jl/stable/",
+        "https://nrel-sienna.github.io/PowerSystems.jl/stable/objects.inv",
+        joinpath(@__DIR__, "inventories", "PowerSystems.toml")
+    ),
+);
 makedocs(;
     modules=[StorageSystemsSimulations],
     format=Documenter.HTML(; prettyurls=haskey(ENV, "GITHUB_ACTIONS")),
+    warnonly=[:missing_docs],
     sitename="StorageSystemsSimulations.jl",
     authors="Jose Daniel Lara, Rodrigo Henriquez-Auba, Sourabh Dalvi",
     pages=Any[p for p in pages],
