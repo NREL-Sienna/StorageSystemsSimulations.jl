5263221ddb311cb8b57d3653e07f8a8db22b62d6
kdayday <kdayday@users.noreply.github.com>
2026-02-04 10:34:33 -0700

    github actions, update deps, final make_tutorials.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,91 +5,20 @@ using DataStructures
 using DocumenterInterLinks
 using Literate
 
+# UPDATE FOR CURRENT MODULE NAME HERE
+const _DOCS_BASE_URL = "https://nrel-sienna.github.io/StorageSystemsSimulations.jl/stable"
+
 links = InterLinks(
     "PowerSimulations" => "https://nrel-sienna.github.io/PowerSimulations.jl/latest/",
 )
 
-# Function to clean up old generated files
-function clean_old_generated_files(dir::String; remove_all_md::Bool=false)
-    if !isdir(dir)
-        @warn "Directory does not exist: $dir"
-        return
-    end
-    if remove_all_md
-        generated_files = filter(f -> endswith(f, ".md"), readdir(dir))
-    else
-        generated_files =
-            filter(f -> startswith(f, "generated_") && endswith(f, ".md"), readdir(dir))
-    end
-    for file in generated_files
-        rm(joinpath(dir, file), force=true)
-        @info "Removed old generated file: $file"
-    end
-end
-
-# Function to add download links to generated markdown
-function add_download_links(content, jl_file, ipynb_file)
-    download_section = """
-
-*To follow along, you can download this tutorial as a [Julia script (.jl)](../$(jl_file)) or [Jupyter notebook (.ipynb)]($(ipynb_file)).*
-
-"""
-    m = match(r"^(#+ .+)$"m, content)
-    if m !== nothing
-        heading = m.match
-        content = replace(content, r"^(#+ .+)$"m => heading * download_section, count=1)
-    end
-    return content
-end
-
-# Process tutorials with Literate
-# Exclude helper scripts that start with "_"
-tutorial_files =
-    filter(x -> occursin(".jl", x) && !startswith(x, "_"), readdir("docs/src/tutorials"))
-if !isempty(tutorial_files)
-    tutorial_outputdir = joinpath(pwd(), "docs", "src", "tutorials", "generated")
-    clean_old_generated_files(tutorial_outputdir; remove_all_md=true)
-    mkpath(tutorial_outputdir)
-
-    for file in tutorial_files
-        @show file
-        infile_path = joinpath(pwd(), "docs", "src", "tutorials", file)
-        execute =
-            occursin("EXECUTE = TRUE", uppercase(readline(infile_path))) ? true : false
-        execute && include(infile_path)
-
-        outputfile = replace("$file", ".jl" => "")
-
-        # Generate markdown
-        Literate.markdown(
-            infile_path,
-            tutorial_outputdir;
-            name=outputfile,
-            credit=false,
-            flavor=Literate.DocumenterFlavor(),
-            documenter=true,
-            postprocess=(
-                content -> add_download_links(content, file, string(outputfile, ".ipynb"))
-            ),
-            execute=execute,
-        )
-
-        # Generate notebook
-        Literate.notebook(
-            infile_path,
-            tutorial_outputdir;
-            name=outputfile,
-            credit=false,
-            execute=false,
-        )
-    end
-end
+include(joinpath(@__DIR__, "make_tutorials.jl"))
+make_tutorials()
 
 pages = OrderedDict(
     "Welcome Page" => "index.md",
     "Tutorials" => Any[
-        "Single State Model" => "tutorials/generated/single_stage_model.md",
-        "Simulation Model" => "tutorials/generated/simulation_tutorial.md",
+        "Single State Model" => "tutorials/generated_single_stage_model.md",
     ],
     # TODO Add sections here once there is content
     # "Explanation" => "explanation/stub.md",
